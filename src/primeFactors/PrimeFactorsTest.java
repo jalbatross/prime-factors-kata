b6bd7cc3cdd6b68c5c800bf739e34470523aa411
jalbatross <ih8u2o@gmail.com>
2018-07-25 21:48:02 -0700

    Made package

---
+++ src/primeFactors/PrimeFactorsTest.java
@@ -0,0 +1,14 @@
+package primeFactors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+class PrimeFactorsTest extends TestCase {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
